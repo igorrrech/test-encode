8b6c3f4021862957825f21b795c39545c8bfd0b8
Igor <igorrrecho@gmail.com>
2025-03-28 11:50:45 +0300

    some decorations

--- cmd/main.go
+++ cmd/main.go
@@ -10,20 +10,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-//	&logic.PersonRepoMock{
-//		Persons: []app.Person{
-//			{Id: 1, Phone: "+79411037894", Email: "igorek@mail.com", FirstName: "Igorek", LastName: "Igorkov"},
-//			{Id: 2, Phone: "+79511036844", Email: "vlad@yandex.com", FirstName: "Vlad", LastName: "Geroin"},
-//			{Id: 3, Phone: "+79511337864", Email: "aye228@gmail.com", FirstName: "Петя", LastName: "Кувалда"},
-//			{Id: 4, Phone: "+79111037894", Email: "lena322@yandex.com", FirstName: "Лена", LastName: "Янач"},
-//		},
-//		Person:       app.Person{Id: 1, Phone: "+79411037894", Email: "igorek@mail.com", FirstName: "Igorek", LastName: "Igorkov"},
-//		CreateError:  nil,
-//		UpdateError:  nil,
-//		GetError:     nil,
-//		GetListError: nil,
-//		DeleteError:  nil,
-//	}
 func main() {
 	cfg := config.MustLoadConfig("./config.json")
 	logger := logrus.New()
